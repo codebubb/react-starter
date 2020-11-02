5f0afd178c0c03b3187def545b48006549d2c150
James Bubb <jpbubb82@gmail.com>
2020-11-02 21:19:48 +0000

    Add Sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
 const path = require('path');
 
 module.exports = {
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index.bundle.js',
-  },
-  devServer: {
-    hot: false,
-    contentBase: path.join(__dirname, 'src'),
-    compress: true,
-    port: 3010,
-    watchContentBase: true,
-    publicPath: '/assets/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js',
+    },
+    entry: './src/index.js',
+    devServer: {
+        hot: false,
+        index: 'index.html',
+        contentBase: path.join(__dirname, '/src'),
+        compress: true,
+        port: 3010,
+        watchContentBase: true,
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
 };
